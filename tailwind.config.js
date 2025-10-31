112780fc9ab1d91a28e108e794e56efcc2fd8c0f
Lucas Lopez Krojzl <70080845+yura9011@users.noreply.github.com>
2025-10-31 13:25:03 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./*.html", // <-- Añade esta línea
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
